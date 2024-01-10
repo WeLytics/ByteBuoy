d460b7293698387f134d99e4c75ef76dd33fdc42
Fabian Keller <fabian.keller@welytics.ai>
2024-01-10 21:35:27 +0100

    added HostedService structure to Agent

--- src/ByteBuoy.Agent/Program.cs
+++ src/ByteBuoy.Agent/Program.cs
@@ -1,12 +1,56 @@
-﻿namespace ByteBuoy.Agent
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace ByteBuoy.Agent
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            // integrate logging
-            // read config
-            // execute jobs
-        }
+		public static async Task<int> Main(string[] args)
+		{
+			// integrate logging
+			// read config
+			// execute jobs
+
+			try
+			{
+				await Host.CreateDefaultBuilder()
+				   // .ConfigureLogging(logging => logging.ClearProviders())
+				   .UseSerilog((hostContext, loggerConfiguration) =>
+				   {
+					   loggerConfiguration
+					   .MinimumLevel.Debug()
+					   .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+					   .MinimumLevel.Override("System", LogEventLevel.Warning)
+					   .WriteTo.File("Setup.log", rollingInterval: RollingInterval.Day);
+				   })
+				   .ConfigureServices((hostContext, services) =>
+				   {
+					   //services.AddSingleton<CreatePackageWorker>();
+					   //services.AddSingleton<SetupConfigurationWorker>();
+					   //services.AddSingleton<SetupStarterWorker>();
+					   //services.AddSingleton<UpdateWorker>();
+					   //services.AddSingleton<SetupWorker>();
+				   })
+				   .Build().RunAsync();
+				//.RunCommandLineApplicationAsync<Program>(args);
+
+
+				if (!System.Diagnostics.Debugger.IsAttached)
+				{
+					Console.Write("Press any key to exit application...");
+					Console.ReadKey();
+				}
+				//return host;
+				return 0;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				Console.ReadKey();
+				return -1;
+			}
+		}
     }
 }
