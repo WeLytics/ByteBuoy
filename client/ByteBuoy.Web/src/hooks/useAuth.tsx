a2b0b5fa4dc076bae164a5664be079df9fce0211
Fabian Keller <fabian.keller@welytics.ai>
2024-04-25 14:29:14 +0200

    fix: metric group auth for admins

--- client/ByteBuoy.Web/src/hooks/useAuth.tsx
+++ client/ByteBuoy.Web/src/hooks/useAuth.tsx
@@ -3,11 +3,12 @@ import { useAuthStore } from  '../store/auth';
 export function useAuth() {
   const { user, userRoles, setUser, setUserRoles, logout } = useAuthStore();
 
-  // Add any additional logic or methods you might need
   const isAuthenticated = Boolean(user);
+  const isAdmin = userRoles?.includes('admin') ?? false;
   
   return {
     isAuthenticated,
+    isAdmin,
     user,
     userRoles,
     setUser,
